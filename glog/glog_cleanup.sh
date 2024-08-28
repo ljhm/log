b390fe91927b585035643bdb6ae1626bcd7a46ee
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-28 19:12:03 +0800

    Add files via upload

--- glog/glog_cleanup.sh
+++ glog/glog_cleanup.sh
@@ -30,19 +30,19 @@ glog_cleanup() {
     fi
 
     # do not delete all of them
-    minimal_count_keep="3"
+    local minimal_count_keep="3"
     if (( max_count_keep < minimal_count_keep )); then
         max_count_keep="$minimal_count_keep"
     fi
 
-    log_files=($(ls -1t "${log_dir}"/*.log."${log_level}".*))
+    local log_files=($(ls -1t "${log_dir}"/*.log."${log_level}".*))
 
     if (( ${#log_files[@]} > $max_count_keep )); then
-        num_to_delete=$((${#log_files[@]} - $max_count_keep))
+        local num_to_delete=$((${#log_files[@]} - $max_count_keep))
 
         # Delete the oldest log files!!!
         for (( i=0; i<num_to_delete; i++ )); do
-            file="${log_files[$((max_count_keep + i))]}"
+            local file="${log_files[$((max_count_keep + i))]}"
             echo "delete $file"
             rm "$file"
         done
@@ -52,9 +52,12 @@ glog_cleanup() {
 
 # $ bash glog_cleanup.sh
 
-LOG_DIR="./log" # path to the log files
-MAX_COUNT_KEEP="3"
+main(){
+    local LOG_DIR="./log" # path to the log files
+    local MAX_COUNT_KEEP="3"
 
-glog_cleanup "$LOG_DIR" "INFO" "$MAX_COUNT_KEEP"
-glog_cleanup "$LOG_DIR" "WARNING" "$MAX_COUNT_KEEP"
-glog_cleanup "$LOG_DIR" "ERROR" "$MAX_COUNT_KEEP"
+    glog_cleanup "$LOG_DIR" "INFO" "$MAX_COUNT_KEEP"
+    glog_cleanup "$LOG_DIR" "WARNING" "$MAX_COUNT_KEEP"
+    glog_cleanup "$LOG_DIR" "ERROR" "$MAX_COUNT_KEEP"
+}
+main
